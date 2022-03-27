3e70bbac7195db465f4522beea3dd26d6db0d980
kirtirajsinh <kirtiraj3738@gmail.com>
2022-03-27 12:31:26 +0530

    Initial Commit

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,30 +1,36 @@
-import { BigInt } from "@graphprotocol/graph-ts"
-import { Contract, tokencreated } from "../generated/Contract/Contract"
-import { ExampleEntity } from "../generated/schema"
+import { BigInt } from "@graphprotocol/graph-ts";
+import { 
+    tokencreated
+} from "../generated/TokenSeed/TokenSeed";
+import {token} from "../generated/schema";
+import { log } from "@graphprotocol/graph-ts";
 
-export function handletokencreated(event: tokencreated): void {
-  // Entities can be loaded from the store using a string ID; this ID
+export function handletokencreated(event:tokencreated): void{
+      // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  let entity = token.load(event.transaction.from.toHex());
 
   // Entities only exist after they have been saved to the store;
   // `null` checks allow to create entities on demand
   if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+    entity = new token(event.transaction.from.toHex());
 
     // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+    entity.count = BigInt.fromI32(0);
   }
 
   // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+//   entity.count = entity.count + BigInt.fromI32(1);
 
   // Entity fields can be set based on event parameters
   entity.tokenaddress = event.params.tokenaddress
-  entity.creator = event.params.creator
+  entity.name = event.params.name;
+  entity.symbol = event.params.symbol;
+  entity.totalSupply = event.params.totalSupply;
+  entity.creator = event.params.creator;
 
   // Entities can be written to the store with `.save()`
-  entity.save()
+  entity.save();
 
   // Note: If a handler doesn't require existing field values, it is faster
   // _not_ to load the entity from the store. Instead, create it fresh with
@@ -41,6 +47,8 @@ export function handletokencreated(event: tokencreated): void {
   // The following functions can then be called on this contract to access
   // state variables and other data:
   //
-  // - contract.children(...)
-  // - contract.create(...)
+  // - contract.getEventDetails(...)
 }
+
+
+
