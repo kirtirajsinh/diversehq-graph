64c043ba5e8680d6da4f12395489d485a185149e
kirtirajsinh <kirtiraj3738@gmail.com>
2022-04-08 23:06:49 +0530

    Update TokenAddress + abi + mapping.ts

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,27 +1,27 @@
 import { BigInt } from "@graphprotocol/graph-ts";
 import { 
-    tokencreated
+    tokencreated as tokencreatedEvent,
 } from "../generated/TokenSeed/TokenSeed";
 import {token} from "../generated/schema";
 import { log } from "@graphprotocol/graph-ts";
 
-export function handletokencreated(event:tokencreated): void{
+export function handletokencreated(event:tokencreatedEvent): void{
       // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = token.load(event.transaction.from.toHex());
-
+  // let entity = token.load(event.transaction.from.toHex());
+  let entity = new token(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  )
   // Entities only exist after they have been saved to the store;
   // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new token(event.transaction.from.toHex());
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0);
-  }
+  // if (!entity) {
+  //   entity = new token(event.transaction.from.toHex());
+  //   // Entity fields can be set using simple assignments
+  //   entity.count = BigInt.fromI32(0);
+  // }
 
   // BigInt and BigDecimal math are supported
-//   entity.count = entity.count + BigInt.fromI32(1);
-
+  // entity.count = entity.count + BigInt.fromI32(1);
   // Entity fields can be set based on event parameters
   entity.tokenaddress = event.params.tokenaddress
   entity.name = event.params.name;
